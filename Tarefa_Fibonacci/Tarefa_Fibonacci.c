9bc36073e2af2ccc5c203f10449843b359908269
Eduardo Tonatto <31291720+EdTonatto@users.noreply.github.com>
2019-09-23 11:47:13 -0300

    Implementado multiplicação de Matriz 2x2

--- Tarefa_Fibonacci/Tarefa_Fibonacci.c
+++ Tarefa_Fibonacci/Tarefa_Fibonacci.c
@@ -4,26 +4,37 @@
 
 void printMatrix(int matrix[2][2]){
 	int i = 0, j = 0;
-	printf("Printando Matriz:\n");
-	printf(" | 0 | 1\n");
-	printf("---------\n");
+	printf("\nPrintando Matriz:\n");
+	printf("  | %4d | %4d\n", i, j);
+	printf("---------------\n");
 	for(i = 0; i < 2; i++){
-		printf("%d| ", i);
+		printf("%2d| ", i);
 		for(j = 0; j < 2; j++){
 			if(j%2 == 0)
-				printf("%d | ", matrix[i][j]);
+				printf("%4d | ", matrix[i][j]);
 			else
-				printf("%d", matrix[i][j]);
+				printf("%4d", matrix[i][j]);
 		}
-		if(i%2 == 0) printf("\n---------\n");
+		if(i%2 == 0) printf("\n---------------\n");
 	}
 }
 
+void multiplyMatrix(int matrixA[2][2], int matrixB[2][2], int matrixAns[2][2]){
+	matrixAns[0][0] = matrixA[0][0] * matrixB[0][0] + matrixA[0][1] * matrixB[1][0];
+	matrixAns[0][1] = matrixA[0][0] * matrixB[0][1] + matrixA[0][1] * matrixB[1][1];
+	matrixAns[1][0] = matrixA[1][0] * matrixB[0][0] + matrixA[1][1] * matrixB[1][0];
+	matrixAns[1][1] = matrixA[1][0] * matrixB[0][1] + matrixA[1][1] * matrixB[1][1];
+}
+
 int main(){
-	int A[2][2] = {{1, 2}, {3, 4}};
-	int B[2][2] = {{-1, 3}, {4, 2}};
+	int A[2][2] = {{5, 5}, {6, 6}};
+	int B[2][2] = {{9, 9}, {9, 9}};
+	int X[2][2];
 	
 	printMatrix(A);
+	printMatrix(B);
+	multiplyMatrix(A, B, X);
+	printMatrix(X);
 	
 	return 0;
 }
