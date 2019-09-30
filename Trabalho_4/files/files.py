60d83b1a08514c94c84f78e56593d52ba044728a
Eduardo Tonatto <edtonatto@gmail.com>
2019-09-30 17:24:11 -0300

    Descrito funções
    Descrevi as funções da files.py e adicionei exemplos

--- Trabalho_4/files/files.py
+++ Trabalho_4/files/files.py
@@ -1,10 +1,21 @@
 import globals
 import utils
 
+#####========================================ENVIO DE ARQUIVO========================================#####
+#Função para chamada criar os diretórios de arquivos enviados e recebidos do usuário logado.
+#Formato do caminho do diretório será:
+##Considerando o login do usuário como Jack, ficará assim:
+###"files/Jack/send_to" para os arquivos enviados.
+###"files/Jack/received_from" para os arquivos recebidos.
 def createFilesPath():
     utils.createPath("files/" + globals.current_user_nickname + "/send_to")
     utils.createPath("files/" + globals.current_user_nickname + "/received_from")
 
+#Irá criar, caso ainda não exista, um arquivo no formato .txt com o nome que estiver informado na variavel de controle global "text_file".
+#O caminho para o arquivo de texto seguirá o padrão comentado na descrição da função files.createFilesPath().
+##A diferença será que irá adicionar, ou reutilizar, um diretório com o nome do destinatário da mensagem
+##Exemplo: Jack está enviando o arquivo com nome Documento.txt para Bill. O caminho do arquivo será:
+###files/Jack/send_to/Bill/Documento.txt
 def createSendFile(content):
     utils.createPath(globals.path_text_file)
     if (not utils.validatePathExistance(globals.path_text_file + "/" + globals.text_file)):
@@ -15,3 +26,4 @@ def createSendFile(content):
         return True
     else:
         print("Arquivo com nome '{name}' não foi criado na pasta com caminho: '{path}'.\nArquivo já existente!".format(name=globals.text_file, path=globals.path_text_file))
+#####========================================ENVIO DE ARQUIVO========================================#####
