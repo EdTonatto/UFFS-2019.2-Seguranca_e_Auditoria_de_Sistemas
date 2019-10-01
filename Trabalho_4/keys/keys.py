491627763773919ff0291b6304f23a73584b3acb
Eduardo Tonatto <edtonatto@gmail.com>
2019-10-01 17:37:41 -0300

    Update keys.py

--- Trabalho_4/keys/keys.py
+++ Trabalho_4/keys/keys.py
@@ -2,39 +2,55 @@ import rsa
 import globals
 import os
 
-def setBitsizeRSA():
-    print("Bitsize atual RSA: {bitsize} bits.".format(bitsize=globals.rsa_key_size))
-    newbitsize = input("Informe o novo bitsize: ")
-    globals.rsa_key_size = int(newbitsize)
-    print("Bitsize atualizado para {bitsize} bits.".format(bitsize=globals.rsa_key_size))
-
-def getPrivateKey():
+def getPrivateKey(): ##DA PRA UNIFICAR EM UMA getKey(usuario, tipochave)
     with open(globals.path_private_key + globals.my_private_key, mode='r') as privatefile:
         keydata = privatefile.read()
         return keydata
 
-def getPublicKey():
+def getPublicKey(): ##DA PRA UNIFICAR EM UMA getKey(usuario, tipochave)
     with open(globals.path_public_key + globals.my_public_key, mode='r') as publicfile:
         keydata = publicfile.read()
         return keydata
 
-def getOtherPublicKey():
+def getOtherPublicKey(): ##DA PRA UNIFICAR EM UMA getKey(usuario, tipochave)
     if (validateKeysFileExistance(globals.other_private_key, globals.other_public_key) == True):
         with open(globals.path_public_key + globals.other_public_key, mode='r') as publicfile:
             keydata = publicfile.read()
             return keydata
 
-def showKeys():
+def getKey(keypath, userkey):
+    if (utils.validatePathExistance(keypath + userkey) == True):
+        with open(keypath + userkey) as key:
+            keydata = key.read()
+            return keydata
+
+def validateKeysFileExistance(pvkeyfile, pbkeyfile): #da pra tentar utilizar o utils.validatePathExistance
+    ret = False
+    try:
+        with open(globals.path_private_key + pvkeyfile, mode='r') as privatefile:
+            with open(globals.path_public_key + pbkeyfile, mode='r') as publicfile:
+                ret = True
+        return ret
+    except:
+        return ret
+
+def setBitsizeRSA(): ##
+    print("Bitsize atual RSA: {bitsize} bits.".format(bitsize=globals.rsa_key_size))
+    newbitsize = input("Informe o novo bitsize: ")
+    globals.rsa_key_size = int(newbitsize)
+    print("Bitsize atualizado para {bitsize} bits.".format(bitsize=globals.rsa_key_size))
+
+def showKeys(): ##
     if (validateKeysFileExistance(globals.my_private_key, globals.my_public_key)  ==  True):
         print(getPrivateKey())
         print(getPublicKey())
         return True
     return False
 
-def showOtherPublicKey():
+def showOtherPublicKey(): ##
     print(getOtherPublicKey())
 
-def deleteKeysFile():
+def deleteKeysFile(): ##
     def printNotFound():
         print("Arquivos '{pathpvk}' e '{pathpbk}' nao foram encontrados.".format(
                                                                             pathpvk=globals.path_private_key + globals.my_private_key,
@@ -53,7 +69,7 @@ def deleteKeysFile():
     except:
         printNotFound()
 
-def generateKeys():
+def generateKeys(): ##
     if (validateKeysFileExistance(globals.my_private_key, globals.my_public_key)  == False):
         (publickey, privatekey) = rsa.newkeys(globals.rsa_key_size)
         with open(globals.path_private_key + globals.my_private_key, 'w') as privatekeyfile:
@@ -64,13 +80,3 @@ def generateKeys():
             publickeyfile.close()
         return True
     return False
-
-def validateKeysFileExistance(pvkeyfile, pbkeyfile):
-    ret = False
-    try:
-        with open(globals.path_private_key + pvkeyfile, mode='r') as privatefile:
-            with open(globals.path_public_key + pbkeyfile, mode='r') as publicfile:
-                ret = True
-        return ret
-    except:
-        return ret
