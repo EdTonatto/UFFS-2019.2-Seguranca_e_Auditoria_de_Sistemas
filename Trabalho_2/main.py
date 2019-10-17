b24db6cb917f8a7920e761be503f1b7354944f59
Eduardo Tonatto <edtonatto@gmail.com>
2019-10-17 20:55:48 -0300

    Cifrando texto, printando dicionario para cifrar

--- Trabalho_2/main.py
+++ Trabalho_2/main.py
@@ -10,5 +10,21 @@ def shuffle_dictionary():
     shuffle(keys)
     return dict(OrderedDict(zip(keys, dictionary.values())))
 
+def print_dictionary():
+    for letter in string.ascii_letters:
+        print("Letra = {letter}, traduzida como {dictionary_letter}".format(letter=letter, dictionary_letter=dictionary[letter]))
+
+def encrypt_text(text):
+    encrypted_text = ""
+    for letter in text:
+        if letter not in string.ascii_letters:
+            encrypted_text = encrypted_text + " "
+        else:
+            encrypted_text = encrypted_text + dictionary[letter]
+    print(encrypted_text)
+
 dictionary = shuffle_dictionary()
-print(dictionary)
+#print_dictionary()
+text = input("Informe um texto para cifrar: ")
+#print(text)
+encrypt_text(text)
