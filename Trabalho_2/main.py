12ce76bd595c8bb4c5a48f0ec83c0a14921df78e
Eduardo Tonatto <edtonatto@gmail.com>
2019-10-17 21:26:31 -0300

    Criado funcao para definir 'histograma'
    Funcao para contar quantas vezes cada letra aparece em um arquivo contendo palavras comuns em ingles

--- Trabalho_2/main.py
+++ Trabalho_2/main.py
@@ -2,17 +2,19 @@ import string
 from collections import OrderedDict
 from random import shuffle
 
-def shuffle_dictionary():
-    dictionary = {}
-    for letter in string.ascii_letters:
-        dictionary[letter] = letter
-    keys = list(dictionary.keys())
-    shuffle(keys)
-    return dict(OrderedDict(zip(keys, dictionary.values())))
+histogram_dict = {}
+encrypt_dict = {}
 
-def print_dictionary():
+def shuffle_dictionary():
     for letter in string.ascii_letters:
-        print("Letra = {letter}, traduzida como {dictionary_letter}".format(letter=letter, dictionary_letter=dictionary[letter]))
+        encrypt_dict[letter] = letter
+    keys = list(encrypt_dict.keys())
+    shuffle(keys)
+    return dict(OrderedDict(zip(keys, encrypt_dict.values())))
+
+def print_encrypt_dict():
+    for letter in string.ascii_letters:
+        print("Letra = {letter}, traduzida como {dictionary_letter}".format(letter=letter, dictionary_letter=encrypt_dict[letter]))
 
 def encrypt_text(text):
     encrypted_text = ""
@@ -20,11 +22,26 @@ def encrypt_text(text):
         if letter not in string.ascii_letters:
             encrypted_text = encrypted_text + " "
         else:
-            encrypted_text = encrypted_text + dictionary[letter]
+            encrypted_text = encrypted_text + encrypt_dict[letter]
     print(encrypted_text)
 
-dictionary = shuffle_dictionary()
-#print_dictionary()
-text = input("Informe um texto para cifrar: ")
+def print_histogram_dict():
+    for letter in string.ascii_letters:
+        print("A letra {letter} apareceu {times} na lista de palavras.".format(letter=letter, times=histogram_dict[letter]))
+
+def create_histogram():
+    for letter in string.ascii_letters:
+        histogram_dict[letter] = 0
+    with open("word_list", 'r') as wlist:
+        for word in wlist:
+            for wletter in word:
+                if wletter in string.ascii_letters:
+                    histogram_dict[wletter] = histogram_dict[wletter] + 1
+
+encrypt_dict = shuffle_dictionary()
+print_encrypt_dict()
+#text = input("Informe um texto para cifrar: ")
 #print(text)
-encrypt_text(text)
+#encrypt_text(text)
+create_histogram()
+print_histogram_dict()
