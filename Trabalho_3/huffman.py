7d340c0fcdd22f5908e449118968700439462a0b
Eduardo <edtonatto@gmail.com>
2019-11-12 13:21:27 -0300

    Criado árvore

--- Trabalho_3/huffman.py
+++ Trabalho_3/huffman.py
@@ -1,4 +1,25 @@
 from collections import Counter
+import heapq
 
 def frequency(text):
     return Counter(text)
+
+def join_node(left, right):
+    frequency = left[0] + right[0]
+    parent = (frequency, '', left, right)
+    return parent
+
+def prefix(frequency):
+    tree = []
+    for char, freq in frequency.items():
+        heapq.heappush(tree, (freq, char))
+
+    print(tree)
+
+    while len(tree) > 1:
+        left = heapq.heappop(tree)
+        right = heapq.heappop(tree)
+        parent = join_node(left, right)
+        heapq.heappush(tree, parent)
+    root = tree[0]
+    return root
--- Trabalho_3/main.py
+++ Trabalho_3/main.py
@@ -4,3 +4,6 @@ text = 'EduardoTonatto'
 
 frequency = frequency(text)
 print(frequency)
+
+prefix = prefix(frequency)
+print(prefix)
